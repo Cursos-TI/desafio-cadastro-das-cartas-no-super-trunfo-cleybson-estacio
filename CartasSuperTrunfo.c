396829dcac7653a0dce534b7255c0c37929cd109
cleybson-estacio <cleybson.estacio@gmail.com>
2025-07-10 01:05:26 +0000

    Adicionar funções para calcular super poder e comparar cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -21,29 +22,44 @@ float calcularPibPerCapita(float pib, int populacao) {
   return pib / populacao;
 }
 
+float calcSuperPoder(unsigned long int populacao, float area, float pib, int qtdTurismo, float pibPerCapita, float densidadeP) {
+  return (float) populacao + area + pib + qtdTurismo + pibPerCapita + densidadeP;
+}
+
+char* compararCartas(float superPoder1, float superPoder2) {
+  short int resultado = superPoder1 > superPoder2;
+  if (resultado == 1) {
+    return "Carta 1 venceu (1)";
+  } else {
+    return "Carta 2 venceu (0)";
+  }
+}
+
 int main()
 {
   // variaveis da carta 1
   char estado1;
   char codigo1[3];
   char nome1[20];
-  int populacao1;
+  unsigned long int populacao1;
   float area1;
   float pib1;
   int qtdPTurismo1;
   float densidadeP1;
   float pibPerCapita1;
+  float superPoder1;
 
   // variaveis da carta 2
   char estado2;
   char codigo2[3];
   char nome2[20];
-  int populacao2;
+  unsigned long int populacao2;
   float area2;
   float pib2;
   int qtdPTurismo2;
   float densidadeP2;
   float pibPerCapita2;
+  float superPoder2;
 
   // Coleta de dados da carta 1
   printf("INFORMAÇÕES DA CARTA 1\n\n\n");
@@ -56,7 +72,7 @@ int main()
   fgets(nome1, 20, stdin);
   nome1[strlen(nome1) - 1] = '\0'; //remove o \n do final da string
   printf("População\n");
-  scanf("%d", &populacao1);
+  scanf("%lu", &populacao1);
   printf("Área (em km²)\n");
   scanf("%f", &area1);
   printf("PIB\n");
@@ -68,17 +84,7 @@ int main()
   // Cálculo da densidade populacional
   densidadeP1 = calcularDensidade(populacao1, area1);
   pibPerCapita1 = calcularPibPerCapita(pib1, populacao1);
-  
-  printf("Carta 1\n");
-  printf("Estado: %c\n", estado1);
-  printf("Código: %s\n", codigo1);
-  printf("Nome da Cidade: %s\n", nome1);
-  printf("População: %d\n", populacao1);
-  printf("Área: %.2f km²\n", area1);
-  printf("PIB: %.2f bilhões de reais\n", pib1);
-  printf("Número de Pontos Turísticos: %d\n", qtdPTurismo1);
-  printf("Densidade Populacional: %.2f habitantes/km²\n", densidadeP1);
-  printf("PIB per capita: %.2f reais\n\n\n\n", pibPerCapita1);
+  superPoder1 = calcSuperPoder(populacao1, area1, pib1, qtdPTurismo1, pibPerCapita1, 1/densidadeP1);
 
   // Coleta de dados da carta 2
   printf("INFORMAÇÕES DA CARTA 2\n\n\n");
@@ -91,7 +97,7 @@ int main()
   fgets(nome2, 20, stdin);
   nome2[strlen(nome2) - 1] = '\0'; //remove o \n do final da string
   printf("População\n");
-  scanf("%d", &populacao2);
+  scanf("%lu", &populacao2);
   printf("Área (em km²)\n");
   scanf("%f", &area2);
   printf("PIB\n");
@@ -103,17 +109,17 @@ int main()
   // Cálculo da densidade populacional
   densidadeP2 = calcularDensidade(populacao2, area2);
   pibPerCapita2 = calcularPibPerCapita(pib2, populacao2);
+  superPoder2 = calcSuperPoder(populacao2, area2, pib2, qtdPTurismo2, pibPerCapita2, 1/densidadeP2);
 
-  printf("Carta 2\n");
-  printf("Estado: %c\n", estado2);
-  printf("Código: %s\n", codigo2);
-  printf("Nome da Cidade: %s\n", nome2);
-  printf("População: %d\n", populacao2);
-  printf("Área: %.2f km²\n", area2);
-  printf("PIB: %.2f bilhões de reais\n", pib2);
-  printf("Número de Pontos Turísticos: %d\n", qtdPTurismo2);
-  printf("Densidade Populacional: %.2f habitantes/km²\n", densidadeP2);
-  printf("PIB per capita: %.2f reais\n", pibPerCapita2);
+  printf("COMPARAÇÃO DE CARTAS:\n");
+  printf("População: %s\n", compararCartas((float) populacao1, (float) populacao2));
+  printf("Área: %s\n", compararCartas(area1, area2));
+  printf("PIB: %s\n", compararCartas(pib1, pib2));
+  printf("Pontos Turísticos: %s\n", compararCartas((float) qtdPTurismo1, (float) qtdPTurismo2));
+  printf("Densidade Populacional: %s\n", compararCartas(1/densidadeP1, 1/densidadeP2));
+  printf("PIB per Capita: %s\n", compararCartas(pibPerCapita1, pibPerCapita2));
+  printf("Super Poder: %s\n", compararCartas(superPoder1, superPoder2));
+
 
   return 0;
 }
